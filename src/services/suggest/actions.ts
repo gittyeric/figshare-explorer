3efd24ca4c8c81d4f0329e6e279a98a7a0260180
Burt <eric@dailyapple.kitchen>
2018-02-08 21:15:22 -0800

    tiny fixes

--- src/services/suggest/actions.ts
+++ src/services/suggest/actions.ts
@@ -1,6 +1,4 @@
 import { NamedReducer, NamedAction } from '../../state';
-import { Page } from '../pagination/pagination-ui';
-import { exec } from 'child_process';
 
 export const storeName = 'suggest';
 
@@ -14,7 +12,7 @@ export enum ItemType {
     REFERENCE = 'refs',
 }
 
-export interface Suggestions<I extends ItemType> {
+export interface Suggestions {
     scores: Score[];
 }
 
@@ -23,19 +21,19 @@ export interface Score {
     score: number;
 }
 
-export interface ItemTypeState<I extends ItemType> {
+export interface ItemTypeState {
     isLoading: boolean;
     isError: boolean;
-    suggested?: Suggestions<I>;
+    suggested?: Suggestions;
 }
 
 export interface SuggestState {
-    articleId: string;
-    papers: ItemTypeState<ItemType.PAPER>;
-    tags: ItemTypeState<ItemType.TAG>;
-    categories: ItemTypeState<ItemType.CATEGORY>;
-    authors: ItemTypeState<ItemType.AUTHOR>;
-    groups: ItemTypeState<ItemType.GROUP>;
+    articleId?: string;
+    papers: ItemTypeState;
+    tags: ItemTypeState;
+    categories: ItemTypeState;
+    authors: ItemTypeState;
+    groups: ItemTypeState;
 }
 
 enum ActionType {
@@ -75,13 +73,13 @@ export const SuggestionTypeFail = (itemType: ItemType): SuggestionTypeFailAction
     };
 };
 
-interface SuggestionTypeDoneAction<I extends ItemType> extends SuggestAction {
-    itemType: I;
-    results: Suggestions<I>;
+interface SuggestionTypeDoneAction extends SuggestAction {
+    itemType: ItemType;
+    results: Suggestions;
 }
 
 export const SuggestionTypeDone = <I extends ItemType>
-    (itemType: I, results: Suggestions<I>): SuggestionTypeDoneAction<I> => {
+    (itemType: I, results: Suggestions): SuggestionTypeDoneAction => {
     return {
         type: ActionType.SuggestionTypeDone,
         storeName: storeName,
@@ -89,6 +87,43 @@ export const SuggestionTypeDone = <I extends ItemType>
         itemType: itemType
     };
 };
+
+const getSuggestionTypeState = <I extends ItemType>
+    (state: SuggestState, itemType: I): ItemTypeState => {
+    switch (itemType) {
+        case ItemType.AUTHOR:
+            return { ...state.authors };
+        case ItemType.CATEGORY:
+            return { ...state.categories };
+        case ItemType.GROUP:
+            return { ...state.groups };
+        case ItemType.PAPER:
+            return { ...state.papers };
+        case ItemType.TAG:
+            return { ...state.tags };
+        default:
+            throw new Error('Unknown ItemType: ' + itemType);
+    }
+};
+
+const setSuggestionTypeState = <I extends ItemType>
+    (state: SuggestState, itemType: I, itemState: ItemTypeState): void => {
+    switch (itemType) {
+        case ItemType.AUTHOR:
+            state.authors = itemState; break;
+        case ItemType.CATEGORY:
+            state.categories = itemState; break;
+        case ItemType.GROUP:
+            state.groups = itemState; break;
+        case ItemType.PAPER:
+            state.papers = itemState; break;
+        case ItemType.TAG:
+            state.tags = itemState; break;
+        default:
+            throw new Error('Unknown ItemType: ' + itemType);
+    }
+};
+
 const suggestionsLoadingReducer = (s: SuggestState, a: SuggestionsLoadingAction) => {
     var newState = { ... s };
     newState.articleId = a.articleId;
@@ -100,7 +135,7 @@ const suggestionsLoadingReducer = (s: SuggestState, a: SuggestionsLoadingAction)
     return newState;
 };
 
-const suggestionTypeDoneReducer = <I extends ItemType>(s: SuggestState, a: SuggestionTypeDoneAction<I>) => {
+const suggestionTypeDoneReducer = (s: SuggestState, a: SuggestionTypeDoneAction) => {
     var newState = { ... s };
     var itemState = getSuggestionTypeState(newState, a.itemType);
     itemState.isError = false;
@@ -120,48 +155,12 @@ const suggestionTypeFailReducer = (s: SuggestState, a: SuggestionTypeFailAction)
     return newState;
 };
 
-const getSuggestionTypeState = <I extends ItemType>
-    (state: SuggestState, itemType: I): ItemTypeState<I> => {
-    switch (itemType) {
-        case ItemType.AUTHOR:
-            return { ...state.authors };
-        case ItemType.CATEGORY:
-            return { ...state.categories };
-        case ItemType.GROUP:
-            return { ...state.groups };
-        case ItemType.PAPER:
-            return { ...state.papers };
-        case ItemType.TAG:
-            return { ...state.tags };
-        default:
-            throw new Error('Unknown ItemType: ' + itemType);
-    }
-};
-
-const setSuggestionTypeState = <I extends ItemType>
-    (state: SuggestState, itemType: I, itemState: ItemTypeState<I>): void => {
-    switch (itemType) {
-        case ItemType.AUTHOR:
-            state.authors = itemState; break;
-        case ItemType.CATEGORY:
-            state.categories = itemState; break;
-        case ItemType.GROUP:
-            state.groups = itemState; break;
-        case ItemType.PAPER:
-            state.papers = itemState; break;
-        case ItemType.TAG:
-            state.tags = itemState; break;
-        default:
-            throw new Error('Unknown ItemType: ' + itemType);
-    }
-};
-
 export const reducer: NamedReducer<SuggestState> = (s: SuggestState, a: NamedAction): SuggestState => {
     switch (a.type) {
         case ActionType.SuggestionsLoading:
             return suggestionsLoadingReducer(s, a as SuggestionsLoadingAction);
         case ActionType.SuggestionTypeDone:
-            return suggestionTypeDoneReducer(s, a as SuggestionTypeDoneAction<ItemType>);
+            return suggestionTypeDoneReducer(s, a as SuggestionTypeDoneAction);
         case ActionType.SuggestionTypeFail:
             return suggestionTypeFailReducer(s, a as SuggestionTypeFailAction);
         default:
--- src/services/suggest/suggest.ts
+++ src/services/suggest/suggest.ts
@@ -1,19 +1,61 @@
 import * as $ from 'jquery';
+import { ItemType, SuggestState, SuggestAction, ItemTypeState, storeName, 
+    reducer, SuggestionsLoading, SuggestionTypeDone, Suggestions, 
+    SuggestionTypeFail } from 'src/services/suggest/actions';
+import { StatefulService } from 'src/services/stateful-service';
 
 const FREQL_API = 'https://papersearch.org';
 
-export enum ItemType {
-    ALL = 'all',
-    TAG = 'tags',
-    CATEGORY = 'cats',
-    GROUP = 'groups',
-    REFERENCE = 'refs',
-}
+const initTypeState: ItemTypeState = {
+    isLoading: false,
+    isError: false
+};
 
-export const suggestArticlesBy = (articleId: Number, byItemType: ItemType, limit = 10) => {
+const initState: SuggestState = {
+    authors: initTypeState,
+    categories: initTypeState,
+    groups: initTypeState,
+    papers: initTypeState,
+    tags: initTypeState
+};
+
+const suggestArticlesByAll = (articleId: string, limit: number) => {
     return $.ajax({
-        url: FREQL_API + '/query/for/' + byItemType,
+        url: FREQL_API + '/query/by/' + ItemType.ALL,
         method: 'get',
         data: { id: articleId, limit: limit }
     });
 };
+
+const suggestItemsForArticle = (articleId: string, itemType: ItemType, limit: number) => {
+    return $.ajax({
+        url: FREQL_API + '/query/for/' + itemType,
+        method: 'get',
+        data: { id: articleId, limit: limit }
+    });
+};
+
+export default class SuggestService extends StatefulService<SuggestState, SuggestAction> {
+
+    constructor() {
+        super(storeName, reducer, initState);
+    }
+
+    loadArticleSuggestions(articleId: string) {
+        this.dispatch(SuggestionsLoading(articleId));
+
+        this.dispatchAfterPending(ItemType.PAPER, suggestArticlesByAll(articleId, 10));
+        this.dispatchAfterPending(ItemType.AUTHOR, suggestItemsForArticle(articleId, ItemType.AUTHOR, 10));
+        this.dispatchAfterPending(ItemType.CATEGORY, suggestItemsForArticle(articleId, ItemType.CATEGORY, 10));
+        this.dispatchAfterPending(ItemType.TAG, suggestItemsForArticle(articleId, ItemType.TAG, 10));
+    }
+
+    private dispatchAfterPending(itemType: ItemType, request: JQueryXHR) {
+        request.then((result) => {
+            this.dispatch(SuggestionTypeDone(itemType, result as Suggestions));
+        }).fail( (error) => {
+            this.dispatch(SuggestionTypeFail(itemType));
+        } );
+    }
+
+}
